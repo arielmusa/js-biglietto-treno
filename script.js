c2c8553371e5ce70bd947079cc379b93aaab3a5c
arielmusa <arielmusainfo@gmail.com>
2025-04-01 16:04:30 +0200

    script.js: cleaning

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // price calc constant
 const ticketPriceByKm = 0.21;
+const discountTwenty = 20;
+const discountFourty = 40;
 
 // collect user input
 let userTravelDistance = parseInt(prompt("Quanti chilometri devi percorrere?"));
 let userAge = parseInt(prompt("Qual è la tua età?"));
-let discount;
 let discountPerc;
 
 // check if stored input is correct
@@ -16,25 +17,20 @@ console.log(typeof userAge);
 // calc ticketPrice based on user input
 let ticketPrice = userTravelDistance * ticketPriceByKm;
 console.log(ticketPrice);
-let discountPrice;
+let finalPrice;
 
 // handle discounts for userAge <18 and >65
 if (userAge < 18) {
-  discount = 20;
-  discountPerc = discount / 100;
-  discountPrice = ticketPrice - ticketPrice * discountPerc;
-  console.log(discountPrice);
+  discountPerc = discountTwenty / 100;
+  finalPrice = ticketPrice - ticketPrice * discountPerc;
+  console.log(finalPrice);
 } else if (userAge > 65) {
-  discount = 40;
-  discountPerc = discount / 100;
-  discountPrice = ticketPrice - ticketPrice * discountPerc;
-  console.log(discountPrice);
+  discountPerc = discountFourty / 100;
+  finalPrice = ticketPrice - ticketPrice * discountPerc;
+  console.log(finalPrice);
 } else {
-  discountPrice = ticketPrice;
+  finalPrice = ticketPrice;
 }
 
 // convert discount price do string
-discountPrice = `€ ${discountPrice.toFixed(2)}`;
-console.log(discountPrice);
-
-alert(`Il prezzo del tuo biglietto è: ${discountPrice}`);
+alert(`Il prezzo del tuo biglietto è: €${finalPrice.toFixed(2)}`);
